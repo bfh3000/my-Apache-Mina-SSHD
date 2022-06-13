b436eaa1d57f78004d7c7f1a8737fa7075ea1fa9
bfh3000 <bfh3000@naver.com>
2022-06-13 19:34:12 +0900

    build setting

--- proxySSH/src/main/java/Main.java
+++ proxySSH/src/main/java/Main.java
@@ -1,6 +1,3 @@
-package org.example;
-
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -9,6 +6,5 @@ public class Main {
 
     public static void main(String[] args) {
         log.debug("Hellow wolrd~~~~~~~~~~~~");
-
     }
 }
---
+++ proxySSH/src/main/java/core/SSHServer.java
@@ -0,0 +1,7 @@
+package core;
+
+public class SSHServer {
+    public static void main(String[] args) {
+
+    }
+}
