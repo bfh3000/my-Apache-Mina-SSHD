64028c7519be2927c257a9a313c011fdec28cb13
bfh3000 <bfh3000@naver.com>
2022-06-15 23:03:54 +0900

    20220615 update

--- proxySSH/src/main/java/core/StartSSHServer.java
+++ proxySSH/src/main/java/core/StartSSHServer.java
@@ -1,48 +1,39 @@
 package core;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.sshd.server.SshServer;
 import org.apache.sshd.server.auth.AsyncAuthException;
 import org.apache.sshd.server.auth.password.PasswordAuthenticator;
 import org.apache.sshd.server.auth.password.PasswordChangeRequiredException;
 import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
 import org.apache.sshd.server.session.ServerSession;
-import org.apache.sshd.server.session.ServerSessionImpl;
-import org.apache.sshd.server.shell.ProcessShellFactory;
+import org.apache.sshd.server.shell.InteractiveProcessShellFactory;
 
 import java.io.IOException;
 
 public class StartSSHServer {
+
+    private static Logger log = LogManager.getLogger();
+
     public static void main(String[] args) {
 
         SshServer sshd = SshServer.setUpDefaultServer();
         sshd.setHost("192.168.5.171");
         sshd.setPort(2022);
-
-//        sshd.setShellFactory(new ProcessShellFactory("/bin/sh", "/C", "..."));
-        sshd.setShellFactory(new ProcessShellFactory("cmd.exe", "/C", "..."));
-
         sshd.setKeyPairProvider(new SimpleGeneratorHostKeyProvider());
-
         sshd.setPasswordAuthenticator(
                 new PasswordAuthenticator() {
                     @Override
                     public boolean authenticate(String username, String password, ServerSession session) throws PasswordChangeRequiredException, AsyncAuthException {
-                        
-                        ServerSession tmp = session;
 
-                        System.out.println("test");
-
-                        /*
-                            1. putty.exe /C {commnad} {C1234123root}
-                            2. C1234123으로 DB조회 후 Client 처리
-                            3. 아.......... shell로 만든 세션 넣어야하는데 어디에 넣는지가 관건이네
-                        */  
-
-                        return true;
+                    return true;
                     }
                 }
 
         );
+
+        sshd.setShellFactory(new InteractiveProcessShellFactory());
 
         try {
             sshd.start();
---
+++ proxySSH/src/main/java/importProxy2st/CustomShell2st.java
@@ -0,0 +1,134 @@
+
+package importProxy2st;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.sshd.common.channel.PtyMode;
+import org.apache.sshd.common.util.logging.AbstractLoggingBean;
+import org.apache.sshd.server.Environment;
+import org.apache.sshd.server.channel.ChannelDataReceiver;
+import org.apache.sshd.server.channel.ChannelSession;
+import org.apache.sshd.server.channel.PuttyRequestHandler;
+import org.apache.sshd.server.session.ServerSession;
+import org.apache.sshd.server.shell.InvertedShell;
+import org.apache.sshd.server.shell.TtyFilterInputStream;
+import org.apache.sshd.server.shell.TtyFilterOutputStream;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HexFormat;
+import java.util.Map;
+import java.util.Objects;
+
+public class CustomShell2st extends AbstractLoggingBean implements InvertedShell {
+    private static final Logger logger = LogManager.getLogger(CustomShell2st.class);
+
+    private ServerSession session;
+    private ChannelSession channelSession;
+    private TtyFilterOutputStream in;
+    private TtyFilterInputStream out;
+    private TtyFilterInputStream err;
+    private ClientDaemon2st client;
+    private OutputStream cin;
+    private ByteArrayOutputStream cout;
+
+    private byte[] outbuf = new byte[8192];
+    private byte[] errbuf = new byte[8192];
+    private ByteArrayInputStream rawout = new ByteArrayInputStream(outbuf);
+    private ByteArrayOutputStream rawin = new ByteArrayOutputStream();
+    private ByteArrayInputStream rawerr = new ByteArrayInputStream(errbuf);
+
+    public CustomShell2st(ClientDaemon2st client) {
+        this.client = client;
+        this.cin = client.channel.getInvertedIn();
+        this.cout = client.out;
+        this.client.setShell(this);
+    }
+
+    @Override
+    public ServerSession getServerSession() {
+        return session;
+    }
+
+    @Override
+    public void setSession(ServerSession session) {
+        this.session = Objects.requireNonNull(session, "No server session");
+    }
+
+    @Override
+    public ChannelSession getServerChannelSession() {
+        return channelSession;
+    }
+
+    @Override
+    public void start(ChannelSession channel, Environment env) throws IOException {
+        this.channelSession = channel;
+
+        Map<PtyMode, ?> modes = resolveShellTtyOptions(env.getPtyModes());
+        out = new TtyFilterInputStream(rawout, modes);
+        err = new TtyFilterInputStream(rawerr, modes);
+        in = new TtyFilterOutputStream(rawin, err, modes);
+
+        channel.setDataReceiver(new ChannelDataReceiver() {
+            @Override
+            public int data(ChannelSession channel, byte[] buf, int start, int len) throws IOException {
+                String stringified = new String(Arrays.copyOfRange(buf, start, start + len));
+                String hex = HexFormat.ofDelimiter(" ").formatHex(stringified.getBytes(StandardCharsets.UTF_8));
+                logger.info(" <<<<<<<< DATA: " + new String(Arrays.copyOfRange(buf, start, start + len)) + " (" + hex + ")", StandardCharsets.UTF_8);
+
+                cin.write(stringified.getBytes(StandardCharsets.UTF_8));
+                cin.flush();
+
+                return 0;
+            }
+
+            @Override
+            public void close() throws IOException {
+            }
+        });
+    }
+
+    protected Map<String, String> resolveShellEnvironment(Map<String, String> env) {
+        return env;
+    }
+
+    // for some reason these modes provide best results BOTH with Linux SSH client and PUTTY
+    protected Map<PtyMode, Integer> resolveShellTtyOptions(Map<PtyMode, Integer> modes) {
+        if (PuttyRequestHandler.isPuttyClient(getServerSession())) {
+            return PuttyRequestHandler.resolveShellTtyOptions(modes);
+        } else {
+            return modes;
+        }
+    }
+
+    @Override
+    public OutputStream getInputStream() {
+        return in;
+    }
+
+    @Override
+    public InputStream getOutputStream() {
+        return out;
+    }
+
+    @Override
+    public InputStream getErrorStream() {
+        return err;
+    }
+
+    @Override
+    public boolean isAlive() {
+        return client.isAlive();
+    }
+
+    @Override
+    public int exitValue() {
+        return 0;
+    }
+
+    @Override
+    public void destroy(ChannelSession channel) {
+        this.client.stop();
+    }
+}
