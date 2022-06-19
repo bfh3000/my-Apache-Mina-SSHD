c94597184d09721970b008fb1f89766550855c18
bfh3000 <bfh3000@naver.com>
2022-06-20 08:43:33 +0900

    add interative socket sample

--- SocketConncet/src/main/java/GateClientSocket.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-import java.net.Socket;
-
-public class GateClientSocket extends Thread{
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost", 2022);
-
-    }
-}
